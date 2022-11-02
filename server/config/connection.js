2b92624d90622d5b81181a28c0c94b840c439d21
FCarter09 <fc_vstate@yahoo.com>
2022-11-02 18:05:41 -0400

    final commit

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pets-ecommerce', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pet-shop', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
